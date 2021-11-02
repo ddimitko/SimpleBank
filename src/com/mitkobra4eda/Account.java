1fdfe3e71b976d78b9bdad9852086a2631054948
mitkobra4eda <mitkovencislavov@gmail.com>
2021-11-02 21:42:10 +0200

    Basic system

---
+++ src/com/mitkobra4eda/Account.java
@@ -0,0 +1,99 @@
+package com.mitkobra4eda;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Account {
+    String userName;
+    String userID;
+    double currentBalance;
+
+    public Account(){
+        Scanner myObj = new Scanner (System.in);
+
+        System.out.println("Welcome to Simple Bank! Let's create your first account!");
+        System.out.println("What is your name?");
+        String name = myObj.nextLine();
+        userName = name;
+        System.out.println();
+        System.out.println("Hello, " + name + "!" + "\nWhat is your UserID?");
+        String uid = myObj.nextLine();
+        userID = uid;
+        System.out.println();
+        System.out.println("Great! Now, how much money do you want to deposit for your starting balance?");
+        double balance = myObj.nextDouble();
+        currentBalance = balance;
+        System.out.println();
+        System.out.println("Username: " + name + "\nUserID: " + uid);
+        System.out.println("Current Balance: " + balance + "$");
+        System.out.println();
+    }
+
+    public void checkAccInfo(){
+        System.out.println("Your Username is: " + userName + "." + "\nYour User ID is: " + userID + "." + "\nYour Balance is: " + currentBalance + "$.");
+    }
+
+    public void deposit(){
+        Scanner menu = new Scanner (System.in);
+        System.out.println("What amount would you like to deposit?");
+        double depositAmount = menu.nextDouble();
+        currentBalance += depositAmount;
+        System.out.println();
+        System.out.println("Successful deposit!");
+        System.out.println("Your new balance is: " + currentBalance + "$");
+    }
+
+    public void withdraw(){
+        Scanner menu = new Scanner (System.in);
+        System.out.println("What amount would you like to deposit?");
+        double withdrawAmount = menu.nextDouble();
+        if(currentBalance > withdrawAmount) {
+            currentBalance -= withdrawAmount;
+            System.out.println("Successful withdrawal!");
+            System.out.println("Your new balance is: " + currentBalance + "$");
+        }
+        else {
+            System.out.println("You don't have sufficient funds to make this transaction!");
+            System.out.println("Desired amount to withdraw: " + withdrawAmount + "$");
+            System.out.println("Current balance: " + currentBalance + "$");
+        }
+    }
+
+    public void mainMenu(){
+        Scanner menu = new Scanner(System.in);
+        boolean exit = false;
+        while(!exit) {
+            System.out.println();
+            System.out.println("What are you going to do today?");
+            System.out.println();
+            System.out.println("Option 1: Check your current Balance and Account info.");
+            System.out.println("Option 2: Make a Deposit.");
+            System.out.println("Option 3: Withdraw money.");
+            System.out.println("Option 4: Exit.");
+            try {
+                int choice = menu.nextInt();
+                switch (choice) {
+                    case 1:
+                        checkAccInfo();
+                        break;
+                    case 2:
+                        deposit();
+                        break;
+                    case 3:
+                        withdraw();
+                        break;
+                    case 4:
+                        exit = true;
+                        System.out.println("Have a nice day!");
+                        break;
+                    default:
+                        System.out.println("Please, pick an option from the list of options!");
+                }
+            }catch(InputMismatchException e){
+                System.out.println("You MUST insert a number!");
+                menu.next();
+            }
+        }
+    }
+}
+
---
+++ src/com/mitkobra4eda/Main.java
@@ -0,0 +1,10 @@
+package com.mitkobra4eda;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Account accountOne = new Account();
+
+        accountOne.mainMenu();
+    }
+}
