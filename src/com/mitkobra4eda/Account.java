c2ec6519775c8cbd9815a4d7f9ee68e3a0f64adb
mitkobra4eda <mitkovencislavov@gmail.com>
2021-11-04 12:54:32 +0200

    *Now the app automatically generates an ID for you

--- src/com/mitkobra4eda/Account.java
+++ src/com/mitkobra4eda/Account.java
@@ -1,6 +1,7 @@
 package com.mitkobra4eda;
 
 import java.util.InputMismatchException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Account {
@@ -15,19 +16,35 @@ public class Account {
         System.out.println("What is your name?");
         String name = myObj.nextLine();
         userName = name;
+        generateID();
         System.out.println();
-        System.out.println("Hello, " + name + "!" + "\nWhat is your UserID?");
+        /*System.out.println("Hello, " + name + "!" + "\nWhat is your UserID?");
         String uid = myObj.nextLine();
-        userID = uid;
+        userID = uid;*/
         System.out.println();
         System.out.println("Great! Now, how much money do you want to deposit for your starting balance?");
         double balance = myObj.nextDouble();
         currentBalance = balance;
         System.out.println();
-        System.out.println("Username: " + name + "\nUserID: " + uid);
+        System.out.println("Username: " + name/* + "\nUserID: " + uid*/);
         System.out.println("Current Balance: " + balance + "$");
         System.out.println();
     }
+    public void generateID() {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        String gen = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        userID = gen;
+    }
+
 
     public void checkAccInfo(){
         System.out.println("Your Username is: " + userName + "." + "\nYour User ID is: " + userID + "." + "\nYour Balance is: " + currentBalance + "$.");
@@ -58,6 +75,7 @@ public class Account {
             System.out.println("Current balance: " + currentBalance + "$");
         }
     }
+
 
     public void mainMenu(){
         Scanner menu = new Scanner(System.in);
